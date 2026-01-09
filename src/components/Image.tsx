6c466ee0353884c0a8002436605888d51215f481
seongminn <berry9901@gmail.com>
2026-01-09 15:09:20 +0900

    init

--- src/components/Image.tsx
+++
@@ -1,9 +0,0 @@
-export const Image = () => {
-  return (
-    <img
-      src="https://statics.goorm.io/gds/resources/images/light/survey.svg"
-      alt="Survey"
-      style={{ height: "120px" }}
-    />
-  );
-};
